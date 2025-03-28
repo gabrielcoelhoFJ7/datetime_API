ecbeacf1489c9bb78d3ebaf3451470551ca79082
gabrielcoelhoFJ7 <microwaycs1@gmail.com>
2025-03-28 15:52:31 -0300

    upd

--- app.py
+++ app.py
@@ -42,6 +42,10 @@ def verificar_data(dia, mes, ano):
         # declaração de variáveis
         data = datetime.datetime(day=int(dia), month=int(mes), year=int(ano))
         agora = datetime.datetime.now()
+        agora = agora.strftime("%d/%m/%Y")
+        agora = datetime.datetime.strptime(agora, "%d/%m/%Y")
+        print(data)
+        print(agora)
         dias_diferenca = 0
         meses_diferenca = 0
         anos_diferenca = 0
@@ -55,19 +59,18 @@ def verificar_data(dia, mes, ano):
             situacao = "futuro"
 
         # diferença
-        if situacao == "passado" or situacao == "futuro":
-            dias_diferenca = int(abs(data - agora).days)
-            meses_diferenca = abs(((data.year - agora.year) * 12) + data.month - agora.month)
-            anos_diferenca = abs(data.year - agora.year)
-            if (data.month >= agora.month) and data.day > agora.day:
-                anos_diferenca = anos_diferenca - 1
+        dias_diferenca = int(abs(data - agora).days)
+        meses_diferenca = abs(((data.year - agora.year) * 12) + data.month - agora.month)
+        anos_diferenca = abs(data.year - agora.year)
+        if (data.month >= agora.month) and data.day > agora.day:
+            anos_diferenca = anos_diferenca - 1
 
-            return jsonify({'anos_diferenca': anos_diferenca,
-                            'dias_diferenca': dias_diferenca,
-                            'meses_diferenca': meses_diferenca,
-                            'situacao': situacao,
-                            'data_agora': agora.strftime('%d/%m/%Y'),
-                            'data_inserida': data.strftime('%d/%m/%Y')})
+        return jsonify({'anos_diferenca': anos_diferenca,
+                        'dias_diferenca': dias_diferenca,
+                        'meses_diferenca': meses_diferenca,
+                        'situacao': situacao,
+                        'data_agora': agora.strftime('%d/%m/%Y'),
+                        'data_inserida': data.strftime('%d/%m/%Y')})
     except ValueError:
         return jsonify({'erro': 'Formato inválido'})
 
